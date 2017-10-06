26642feacf654d1122c8cb32e41e84fecd3e34ae
Colter McQuay <colter.mcquay@gmail.com>
2017-10-06 15:33:07 -0700

    Adding debugging application

---
+++ src/FlexLib.Debugging/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Flex.Smoothlake.FlexLib;
+
+namespace FlexLib.Debugging
+{
+    class MainClass
+    {
+
+        static void Main(string[] args)
+        {
+
+            API.RadioAdded += async (radio) =>
+            {
+                radio.Connect();
+                var panAdapters = await WaitForPanadaptersAsync(radio);
+                var panAdapter = panAdapters.FirstOrDefault();
+                if (panAdapter == null)
+                {
+                    panAdapter = await radio.CreatePanadapterAsync(0, 0);
+
+					// Remove the slices for our new pan adapter
+					radio.SliceList
+						 .Where(s => s.Panadapter == panAdapter)
+						 .ToList()
+						 .ForEach(s => s.Remove(true));
+                }
+
+
+
+                panAdapter.DAXIQChannel = 1;
+
+                var iq = radio.FindIQStreamByDAXIQChannel(1);
+                if (iq == null)
+                {
+                    iq = await radio.CreateIQStreamAsync(1);
+                }
+
+                iq.DataReady += IQ_Data_Received;
+                iq.SampleRate = 192000;
+
+
+            };
+            API.Init();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+
+        private static async Task<List<Panadapter>> WaitForPanadaptersAsync(Radio radio, int waitMs = 2000)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource(waitMs);
+            var token = cts.Token;
+            return await Task.Run(() =>
+            {
+                // Wait for panadapters
+                while (radio.PanadapterList.Count == 0)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        return new List<Panadapter>();
+                    }
+                }
+                // Return the first panAdapter
+                return radio.PanadapterList;
+            }, token);
+        }
+
+        private static void IQ_Data_Received(IQStream iq_stream, float[] data)
+        {
+            Console.Write("Yes sir");
+        }
+    }
+}
